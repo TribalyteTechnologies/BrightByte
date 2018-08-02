6ae8e7edf445ca968f214e675aee822e54a8131d
ARR <arodriguez@tribalyte.com>
2018-08-02 17:02:56 +0200

    Number of reviews are showed on ranking view

--- src/models/user-reputation.model.ts
+++ src/models/user-reputation.model.ts
@@ -3,10 +3,12 @@ import { AppConfig } from "../app.config";
 export class UserReputation { 
     public email: string; 
     public reputation: number;
+    public numReviews: number;
     public static fromSmartContract(userVals: Array<any>): UserReputation{ 
         let user = new UserReputation(); 
         user.email = userVals[0]; 
         user.reputation = userVals[1] / AppConfig.SCORE_DIVISION_FACTOR; 
+        user.numReviews = userVals[2];
         return user; 
     } 
 } 
