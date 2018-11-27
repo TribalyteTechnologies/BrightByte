a1db522e9ec38a97c798fc1256f94ecae5cabdd6
Rafael Barriuso <rbarriuso@tribalyte.com>
2018-11-27 20:20:59 +0100

    Fixed linter warnings

--- src/components/CommitCard/commit-card.component.ts
+++ src/components/CommitCard/commit-card.component.ts
@@ -4,11 +4,13 @@ import { UserCommit } from "../../models/user-commit.model";
 
 @Component({
     selector: "commit-card",
-    templateUrl: " commit-card.component.html ",
+    templateUrl: "commit-card.component.html",
     styles: ["commit-card.component.scss"]
  })
-
+ 
 export class CommitCard {
+    
     @Input()
-    public commit: UserCommit;  
+    public commit: UserCommit;
+
 }
--- src/components/ReviewCard/review-card.component.ts
+++ src/components/ReviewCard/review-card.component.ts
@@ -4,11 +4,12 @@ import { UserCommit } from "../../models/user-commit.model";
 
 @Component({
     selector: "review-card",
-    templateUrl: " review-card.component.html ",
+    templateUrl: "review-card.component.html",
     styles: ["review-card.component.scss"]
  })
-
 export class ReviewCard {
+    
     @Input()
     public commit: UserCommit;
+
 }
