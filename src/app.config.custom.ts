372cde8b9446ac2ea01855827ad0247b00caafde
Ronny Demera <rdemera@tribalyte.com>
2019-01-28 16:34:07 +0100

    Solved issue on review page
    *Solved the duplication of comments when a commit was selected on commit pag
    *Updated list of nodes

--- src/app.config.custom.ts
+++ src/app.config.custom.ts
@@ -11,42 +11,6 @@ export default {
       gasPrice: 0,
       netId: 82584648528,
       urlNode: "1"
-    },
-    {
-      gasLimit: 2000000,
-      gasPrice: 0,
-      netId: 82584648528,
-      urlNode: "2"
-    },
-    {
-      gasLimit: 2000000,
-      gasPrice: 0,
-      netId: 82584648528,
-      urlNode: "HTTP://5.56.62.209:22000"
-    },
-    {
-      gasLimit: 2000000,
-      gasPrice: 0,
-      netId: 82584648528,
-      urlNode: "4"
-    },
-    {
-      gasLimit: 2000000,
-      gasPrice: 0,
-      netId: 82584648528,
-      urlNode: "5"
-    },
-    {
-      gasLimit: 2000000,
-      gasPrice: 0,
-      netId: 82584648528,
-      urlNode: "6"
-    },
-    {
-      gasLimit: 2000000,
-      gasPrice: 0,
-      netId: 82584648528,
-      urlNode: "7"
     }
   ]
 };
