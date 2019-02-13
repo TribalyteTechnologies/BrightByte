ca67e0b27bcc43cc0774d06fb49c56746c2b73e7
elopez <elopez@tribalyte.com>
2019-02-13 10:40:11 +0100

    Migration Removed

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,6 @@
 import { Component } from "@angular/core";
-import { NavController, LoadingController } from "ionic-angular";
 import { TranslateService } from "@ngx-translate/core";
 import { NewuserPage } from "../newuser/newuser";
-import { AlertController } from "ionic-angular";
 import { ILogger, LoggerService } from "../../core/logger.service";
 import { Web3Service } from "../../core/web3.service";
 import { LoginService } from "../../core/login.service";
@@ -15,6 +13,7 @@ import { SpinnerService } from "../../core/spinner.service";
 import { UserLoggerService } from "../../domain/user-logger.service";
 import { Account } from "web3/types";
 import { AppConfig } from "../../app.config";
+import { NavController } from "ionic-angular";
 
 @Component({
     selector: "page-login",
@@ -41,8 +40,6 @@ export class LoginPage {
         private loginService: LoginService,
         private userLoggerService: UserLoggerService,
         private spinnerService: SpinnerService,
-        private alertCtrl: AlertController,
-        private loadingCtrl: LoadingController,
         loggerSrv: LoggerService,
         appVersionSrv: AppVersionService
     ) {
@@ -139,66 +136,6 @@ export class LoginPage {
 
     public register() {
         this.navCtrl.push(NewuserPage);
-    }
-
-    public migrate(pass){
-        let alert = this.alertCtrl.create({
-            title: "Migration",
-            subTitle: "The migration has been successful",
-            buttons: ["Accept"]
-        });
-
-        let alertError = this.alertCtrl.create({
-            title: "Error",
-            subTitle: "The migration has failed",
-            buttons: ["Accept"]
-        });
-
-        let loader = this.loadingCtrl.create();
-        loader.present();
-        try {
-            this.log.d("File imported: ", this.text);
-            if (this.text === undefined) {
-                this.log.e("File not loaded");
-                this.translateService.get("app.fileNotLoaded").subscribe(
-                    msg => {
-                        loader.dismiss();
-                        this.msg = msg;
-                    });
-            } else {
-                let account = this.web3Service.getWeb3().eth.accounts.decrypt(this.text, pass);
-                this.log.d("Imported account from the login file: ", account);
-                this.loginService.setAccount(account);
-                this.contractManager.init(account, 0)
-                    .then(() => {
-                        return this.contractManager.getUserMigration();
-                    }).then((result) => {
-                        this.migrationDone = true;
-                        this.userLoggerService.setMigration();
-                        loader.dismiss();
-                        alert.present();
-                    })
-                    .catch((e) => {
-                        window.alert(e);
-                        this.translateService.get("app.noRpc").subscribe(
-                            msg => {
-                                loader.dismiss();
-                                this.msg = msg;
-                            });
-                        this.log.e("ERROR getting user or checking if this user has already set his profile: ", e);
-                        loader.dismiss();
-                        alertError.present();
-                    });
-
-            }
-        } catch (e) {
-            this.translateService.get("app.wrongPassword").subscribe(
-                msg => {
-                    this.msg = msg;
-                    this.log.e(msg, e);
-                    loader.dismiss();
-                });
-        }
     }
 
     private checkNodesAndOpenHomePage (account: Account, currentNodeIndex: number): Promise<boolean> {
