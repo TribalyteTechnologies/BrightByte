3e703c3ff762a0df7248ffdb0b5aa3f9a534062e
Álvaro Gómez Haro <agomez@tribalyte.com>
2019-04-01 12:23:13 +0200

    New custom rating icons and functionality. (Only for demo)

--- src/components/custom-rating/custom-rating.component.ts
+++ src/components/custom-rating/custom-rating.component.ts
@@ -25,26 +25,64 @@ export class CustomRating {
     public small: boolean = false;
 
     @Input()
-    public starPadding: boolean = false;
+    public iconPadding: boolean = false;
 
-    public starIds: number[];
-    public stars: string[];
+    @Input()
+    public ratingType = "codeQuality";
+
+    public iconColor = "stars-color";
+
+    public iconIds: number[];
+    public icons: string[];
+    public ghosted: boolean;
+
+    private difficultyIcons = ["bb-boy", "bb-graduation", "bb-crown"];
+    private confidenceIcons = ["bb-gears-1", "bb-gears-2", "bb-gears-3"];
+    private currentIcons = [""];
 
     public ngOnInit(){
-        this.starIds = new Array<number>(this.max);
-        this.stars = new Array<string>(this.max);
-        for(let i = 0; i < this.max; i++){
-            this.starIds[i] = i;
-            this.stars[i] = "star-outline";
+        this.iconIds = new Array<number>(this.max);
+        this.icons = new Array<string>(this.max);
+        
+        switch (this.ratingType) {
+            case "codeQuality":
+                for(let i = 0; i < this.max; i++){
+                    this.iconIds[i] = i;
+                    this.icons[i] = "star-outline";
+                }
+                break;
+            case "difficulty":
+                this.iconColor = "gears-color";
+                this.currentIcons = this.difficultyIcons;
+                break;
+            case "reviewerExperience":
+                this.iconColor = "confidence-color";
+                this.currentIcons = this.confidenceIcons;
+                break;
+            default:
+                break;
         }
+
+        for(let i = 0; i < this.max; i++){
+            this.iconIds[i] = i;
+            this.icons[i] = this.currentIcons[i];
+        }
+        
         this.setReputation(this.rate - 1);
     }
 
     public setReputation(value: number) {
-        for(let i = 0; i < this.max; i++){
-            this.stars[i] = i <= value ? "star" : "star-outline";
+
+        if (this.ratingType === "codeQuality"){
+            for(let i = 0; i < this.max; i++){
+                this.icons[i] = i <= value ? "star" : "star-outline";
+            }
+        }else{
+            for(let i = 0; i < this.max; i++){
+                this.icons[i] = i === value ? this.currentIcons[i] : this.currentIcons[i] + "-ghosted";
+            }
         }
-        
+   
         this.rate = value;
         this.value.next((this.rate + 1) * 100);
     }
