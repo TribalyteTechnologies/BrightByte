d8719ad6e7db96fc9ed2dffe5a345fb446f85f04
Ronny Demera <rdemera@tribalyte.com>
2019-04-05 12:49:26 +0200

    Minor change

--- src/models/user-commit.model.ts
+++ src/models/user-commit.model.ts
@@ -3,20 +3,20 @@ import { UserDetails } from "./user-details.model";
 import { FormatUtils } from "../core/format-utils";
 
 export class UserCommit { 
-    public url: string; 
-    public urlHash: string;
-    public title: string;
-    public author: string;
-    public project: string; 
-    public isPending: boolean; 
-    public isReadNeeded: boolean;
-    public score: number;
-    public creationDateMs: number;
-    public lastModificationDateMs: number;
-    public numberReviews: number;
-    public currentNumberReviews: number;
+    public url = ""; 
+    public urlHash = "";
+    public title = "";
+    public author = "";
+    public project = ""; 
+    public isPending = false; 
+    public isReadNeeded = false;
+    public score = 0;
+    public creationDateMs = 0;
+    public lastModificationDateMs = 0;
+    public numberReviews = 0;
+    public currentNumberReviews = 0;
     public reviewers: UserDetails[][];
-    public reviewsAlreadyDone: string[];
+    public reviewsAlreadyDone = new Array<string>();
 
     public static fromSmartContract(commitVals: Array<any>, isPending: boolean): UserCommit{ 
         let commit = new UserCommit();
@@ -34,22 +34,7 @@ export class UserCommit {
             commit.currentNumberReviews = commitVals[7];
             commit.score = Math.round(commitVals[8] / AppConfig.SCORE_DIVISION_FACTOR) ;
             commit.reviewers = [];
-            commit.reviewsAlreadyDone =  new Array<string>();   
-        } else {
-            commit.url = "";
-            commit.urlHash = "";
-            commit.title = "";
-            commit.author = "";
-            commit.project = "";
-            commit.isPending = false;
-            commit.creationDateMs = 0;
-            commit.lastModificationDateMs = 0;
-            commit.isReadNeeded = false;
-            commit.numberReviews = 0;
-            commit.currentNumberReviews = 0;
-            commit.score = 0 ;
-            commit.reviewers = [];
-            commit.reviewsAlreadyDone =  new Array<string>();  
+            commit.reviewsAlreadyDone =  new Array<string>();
         }
         return commit;
     }
