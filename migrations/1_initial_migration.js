0673eb9f6e1d44c1f0b5d24ad6d77e1c294eaa28
Ronny Demera <rdemera@tribalyte.com>
2019-05-09 13:24:23 +0200

    Updated the way to deploy new smart contracts on Telius

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -6,12 +6,9 @@ var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer, network, addresses) {
 	const config = TruffleConfig.networks[network];
-
+	
 	if (process.env.ACCOUNT_PASSWORD) {
 		const web3 = new Web3(new Web3.providers.HttpProvider("http://" + config.host + ":" + config.port));
-
-		console.log(">> Unlocking account " + config.from + " with pass: " + process.env.ACCOUNT_PASSWORD);
-		web3.eth.personal.unlockAccount(config.from, process.env.ACCOUNT_PASSWORD, 36000);
 	}
 
 	console.log(">> Deploying migrations contract");
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,9 @@ var Commits = artifacts.require("./Commits.sol");
 var Root = artifacts.require("./Root.sol");
 var Reputation = artifacts.require("./Reputation.sol");
 
+const INITIAL_SEASON_INDEX = 2;
+const INITIAL_SEASON_TIMESTAMP = 1550047598;
+
 module.exports = function(deployer) {
     console.log("Deploying Bright contract");
     deployer.deploy(Bright)
@@ -14,6 +17,6 @@ module.exports = function(deployer) {
         return deployer.deploy(Reputation);
     }).then(function() {
         console.log("Deploying Root contract. Addresses: ", [Bright.address, Commits.address, Reputation.address]);
-        return deployer.deploy(Root, Bright.address, Commits.address, Reputation.address);
+        return deployer.deploy(Root, Bright.address, Commits.address, Reputation.address, INITIAL_SEASON_INDEX, INITIAL_SEASON_TIMESTAMP);
     });
 };
