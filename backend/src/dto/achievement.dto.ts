8ee37451431a7081027ed0a985ce61f8d585f9e8
iruiz <iruiz@tribalyte.com>
2019-07-18 09:18:30 +0200

    Added achievement model.

---
+++ backend/src/dto/achievement.dto.ts
@@ -0,0 +1,15 @@
+export class AchievementDto { 
+    public id: string
+    public title: string;
+    public quantity: number; 
+    public parameter: string; 
+    public iconPath: string;
+
+    public constructor(id: string, title?: string, quantity?: number, parameter?: string, iconPath?: string){
+        this.id = id;
+        this.title = title;
+        this.quantity = quantity;
+        this.parameter = parameter;
+        this.iconPath = iconPath;
+    }
+}
