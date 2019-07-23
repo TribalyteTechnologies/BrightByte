15e9ab41b8831444658bfe68e6a053adf7eb8bfe
iruiz <iruiz@tribalyte.com>
2019-07-23 09:35:15 +0200

    Adapted DTOs to new logic.

--- backend/src/dto/achievement.dto.ts
+++ backend/src/dto/achievement.dto.ts
@@ -1,11 +1,11 @@
-export class AchievementDto { 
-    public id: string;
+export class AchievementDto {
+    public id: number;
     public title: string;
-    public quantity: number; 
-    public parameter: string; 
+    public quantity: number;
+    public parameter: string;
     public iconPath: string;
 
-    public constructor(id: string, title?: string, quantity?: number, parameter?: string, iconPath?: string){
+    public constructor(title: string, quantity: number, parameter: string, iconPath: string, id?: number) {
         this.id = id;
         this.title = title;
         this.quantity = quantity;
--- backend/src/dto/user.dto.ts
+++ backend/src/dto/user.dto.ts
@@ -2,10 +2,12 @@ export class UserDto{
     public id: string;
     public commitCount: number;
     public reviewCount: number;
+    public obtainedAchievements: number[];
 
-    public constructor(id: string, commitCount: number, reviewCount: number){
+    public constructor(id: string, commitCount: number, reviewCount: number, obtainedAchievements?: number[]){
         this.id = id;
         this.commitCount = commitCount;
         this.reviewCount = reviewCount;
+        this.obtainedAchievements = obtainedAchievements;
     }
 }
