b42a4f5002688d02968462741e86b396222cda3a
iruiz <iruiz@tribalyte.com>
2019-07-23 09:38:13 +0200

    Updated dependencies
    Necessary to "npm install".

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { Module } from "@nestjs/common";
+import { Module, HttpModule } from "@nestjs/common";
 import { BackendConfig } from "src/backend.config";
-import { UserDatabaseController } from "../controllers/user-database.controller";
 import { UserDatabaseService } from "../services/user-database.service";
 import { AchievementDatabaseService } from "../services/achievement-database.service";
+import { UserDatabaseController } from "../controllers/user-database.controller";
+import { AchievementDatabaseController } from "../controllers/achievement-database.controller";
 import { LoggerService } from "../logger/logger.service";
 import { Web3Service } from "../services/web3.service";
-import { EventHandlerService } from "../services/eventHandler.service";
-
+import { EventHandlerService } from "../services/event-handler.service";
 
 @Module({
-    imports: [BackendConfig],
-    controllers: [UserDatabaseController],
+    imports: [BackendConfig,
+        HttpModule
+    ],
+    controllers: [UserDatabaseController,
+        AchievementDatabaseController
+    ],
     providers: [UserDatabaseService,
         AchievementDatabaseService,
         Web3Service,
         EventHandlerService,
         { provide: LoggerService, useFactory: () => new LoggerService(BackendConfig.LOG_DEBUG) }
-        ] 
+    ]
 })
 export class AppModule { }
