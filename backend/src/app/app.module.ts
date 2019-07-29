e86e402124723417a79a6de68e5cb79db17e75ff
iruiz <iruiz@tribalyte.com>
2019-07-29 12:14:10 +0200

    Removed unused controller.

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,28 +1,27 @@
-import { Module, HttpModule } from "@nestjs/common";
-import { BackendConfig } from "../backend.config";
+import { Module } from "@nestjs/common";
+import { BackendConfig } from "src/backend.config";
 import { UserDatabaseService } from "../services/user-database.service";
 import { AchievementDatabaseService } from "../services/achievement-database.service";
 import { UserDatabaseController } from "../controllers/user-database.controller";
-import { AchievementDatabaseController } from "../controllers/achievement-database.controller";
 import { LoggerService } from "../logger/logger.service";
 import { Web3Service } from "../services/web3.service";
 import { EventHandlerService } from "../services/event-handler.service";
 import { DispatcherService } from "../services/dispatcher.service";
-import { EventDatabaseService } from "../services/event-database.service";
+import { EventDatabaseService } from "src/services/event-database.service";
+import { CoreDatabaseService } from "src/services/core-database.service";
 
 @Module({
     imports: [
-        BackendConfig,
-        HttpModule
+        BackendConfig
     ],
     controllers: [
-        UserDatabaseController,
-        AchievementDatabaseController
+        UserDatabaseController       
     ],
     providers: [
         UserDatabaseService,
         AchievementDatabaseService,
         EventDatabaseService,
+        CoreDatabaseService,
         Web3Service,
         DispatcherService,
         EventHandlerService,
--- backend/src/controllers/achievement-database.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { AchievementDatabaseService } from "../services/achievement-database.service";
-
-@Controller("achievements")
-export class AchievementDatabaseController {
-    public constructor(private achievementDatabaseService: AchievementDatabaseService) { }
-}
