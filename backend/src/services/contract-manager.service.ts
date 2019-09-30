269ff055f8a76628006a353f13778b9d403166fd
Álvaro Gómez Haro <agomez@tribalyte.com>
2019-09-30 08:17:39 +0200

    Added initialization observable to contract manager service

--- backend/src/services/contract-manager.service.ts
+++ backend/src/services/contract-manager.service.ts
@@ -6,6 +6,7 @@ import Web3 from "web3";
 import { UserDetailsDto } from "../dto/user-details.dto";
 import { Observable, from, forkJoin } from "rxjs";
 import { flatMap, map, tap } from "rxjs/operators";
+import { AxiosResponse } from "axios";
 
 interface ITrbSmartContact {
     [key: string]: any;
@@ -19,6 +20,7 @@ export class ContractManagerService {
     private web3: Web3;
     private contracts: Array<ITrbSmartContact>;
     private brightContractAbi;
+    private initObs: Observable<AxiosResponse<any>>;
 
     public constructor(
         private httpSrv: HttpService,
@@ -29,19 +31,22 @@ export class ContractManagerService {
         this.web3 = web3Service.getWeb3();
         this.web3Service = web3Service;
         this.contracts = new Array<ITrbSmartContact>();
-        this.init();
+        this.initObs = this.init();
+        this.initObs.subscribe(() => this.log.d("The Contract Manager Service is ready"));
     }
 
     public getAllUserData(): Observable<Array<UserDetailsDto>> {
-        return this.getUsersAddress().pipe(flatMap((usersAddress: String[]) => {
-            this.log.d("The number of users is " + usersAddress.length);
-            let observables = usersAddress.map(userAddress => from(
-                this.contracts[0].methods.getUserGlobalReputation(userAddress).call()
-            ).pipe(
-                map((userData: any[]) => UserDetailsDto.fromSmartContract(userData))
-            ));
-            return forkJoin(observables);
-        }));
+        return this.initObs.pipe(
+            flatMap(() => this.getUsersAddress()),
+            flatMap((usersAddresses: any[]) => {
+                let observables = usersAddresses.map(userAddress => from(
+                    this.contracts[0].methods.getUserGlobalReputation(userAddress).call()
+                ).pipe(
+                    map((userData: any[]) => UserDetailsDto.fromSmartContract(userData))
+                ));
+                return forkJoin(observables);
+            })
+        );
     }
 
     private getUserNumber(): Observable<Number> {
@@ -52,12 +57,12 @@ export class ContractManagerService {
         return from<Array<String>>(this.contracts[0].methods.getUsersAddress().call());
     }
 
-    private init() {
+    private init(): Observable<AxiosResponse<any>> {
         this.log.d("Initializing Contract Manager Service");
-        this.httpSrv.get(BackendConfig.BRIGHT_CONTRACT_URL).subscribe(response => {
+        return this.httpSrv.get(BackendConfig.BRIGHT_CONTRACT_URL).pipe(tap(response => {
             this.brightContractAbi = response.data;
             this.contractAddressBright = this.brightContractAbi.networks[BackendConfig.netId].address;
             this.contracts.push(new this.web3.eth.Contract(this.brightContractAbi.abi, this.contractAddressBright));
-        });
+        }));
     }
 }
