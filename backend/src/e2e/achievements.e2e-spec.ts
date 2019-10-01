b54975d412f24e82bafb109e0f4a9e96e5cd768e
Álvaro Gómez Haro <agomez@tribalyte.com>
2019-10-01 08:24:08 +0200

    Solved #18cfc57 comments

--- backend/src/e2e/achievements.e2e-spec.ts
+++ backend/src/e2e/achievements.e2e-spec.ts
@@ -4,32 +4,32 @@ import { AppModule } from "../app/app.module";
 import { INestApplication } from "@nestjs/common";
 
 describe("Achievements", () => {
-  const USER_ADDRESS = "0x05e877764bA5f17926c3a918B58255BB1C42aDF4";
-  const ACHIEVEMENT_QUERY = "/database/achievements/";
-  const EXPECTED_ACHIEVEMENTS_LENGTH = 2;
-  let app: INestApplication;
+    const USER_ADDRESS = "0x05e877764bA5f17926c3a918B58255BB1C42aDF4";
+    const ACHIEVEMENT_QUERY = "/database/achievements/";
+    const EXPECTED_ACHIEVEMENTS_LENGTH = 2;
+    let app: INestApplication;
 
-  beforeAll(async () => {
-    const module = await Test.createTestingModule({
-      imports: [AppModule]
-    })
-      .compile();
+    beforeAll(async () => {
+        const module = await Test.createTestingModule({
+            imports: [AppModule]
+        })
+            .compile();
 
-    app = module.createNestApplication();
-    await app.init();
-  });
+        app = module.createNestApplication();
+        await app.init();
+    });
 
-  it("/GET achievements", () => {
-    return request(app.getHttpServer())
-      .get(ACHIEVEMENT_QUERY + USER_ADDRESS)
-      .expect(200)
-      .then(res => {
-        expect(res.body.data.length).toBe(EXPECTED_ACHIEVEMENTS_LENGTH);
-      });
-      
-  });
+    it("/GET achievements", () => {
+        return request(app.getHttpServer())
+            .get(ACHIEVEMENT_QUERY + USER_ADDRESS)
+            .expect(200)
+            .then(res => {
+                expect(res.body.data.length).toBe(EXPECTED_ACHIEVEMENTS_LENGTH);
+            });
 
-  afterEach(async () => {
-    let a = await app.close();
-  });
+    });
+
+    afterEach(async () => {
+        let a = await app.close();
+    });
 });
