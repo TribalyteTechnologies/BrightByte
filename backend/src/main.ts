8f666c248bcfa1aeca3cacec0b220661fce36d4b
Álvaro <agomez@tribalyte.com>
2019-12-02 13:16:37 +0100

    Minor backend correction

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,6 @@ import { BackendConfig } from "./backend.config";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({"origin": BackendConfig.WEBAPP_URL});
-    await app.listen(BackendConfig.BRIGHTBYTE_DB_PORT);
+    await app.listenAsync(BackendConfig.BRIGHTBYTE_DB_PORT);
 }
 bootstrap();
