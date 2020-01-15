52e6b8cf01cbae852b31f0fa6d1d0e6c06ed9665
Álvaro <agomez@tribalyte.com>
2020-01-15 11:10:10 +0100

    Implemented blockchain events achievement initialization +minor refactor

--- backend/src/services/event-handler.service.ts
+++ backend/src/services/event-handler.service.ts
@@ -16,6 +16,15 @@ export class EventHandlerService {
     private readonly COMMIT = "Commit";
     private readonly REVIEW = "Review";
     private readonly SEASON = "Season";
+    private readonly LISTENER_CLOSE_EVENT = "close";
+    private readonly LISTENER_END_EVENT = "end";
+    private readonly USER_HASH = "userHash";
+    private readonly NUMBER_COMMITS = "numberOfCommits";
+    private readonly NUMBER_REVIEWS = "numberOfReviews";
+    private readonly CURRENT_SEASON = "currentSeason";
+    private readonly LATEST = "latest";
+    private readonly FIRST_BLOCK = 0;
+
 
     private contractAddress: string;
     private contract: ITrbSmartContact;
@@ -46,14 +55,14 @@ export class EventHandlerService {
                 this.log.d("Web3 Connection established");
                 this.contractAddress = this.jsonContractData.networks[BackendConfig.NET_ID].address;
                 this.contract = new this.web3.eth.Contract(this.jsonContractData.abi, this.contractAddress);
-                this.eventsSubscription();
+                this.eventsSubscription(true);
             }).catch(e => {
                 this.log.e("Not able to open a connection " + e);
             });
         });
     }
 
-    public registerNewListener(type: string) {
+    public registerNewListener(type: string, initialization = false) {
         this.log.d("New Subscription");
         let callback = (error, event) => {
             if (event) {
@@ -62,33 +71,37 @@ export class EventHandlerService {
                 switch (type) {
                     case this.COMMIT:
                         newEvent = new CommitEventDto(
-                            event.returnValues["userHash"], parseInt(event.returnValues["numberOfCommits"]));
+                            event.returnValues[this.USER_HASH], parseInt(event.returnValues[this.NUMBER_COMMITS]));
                         break;
                     case this.REVIEW:
                         newEvent = new ReviewEventDto(
-                            event.returnValues["userHash"], parseInt(event.returnValues["numberOfReviews"]));
+                            event.returnValues[this.USER_HASH], parseInt(event.returnValues[this.NUMBER_REVIEWS]));
                         break;
                     case this.SEASON:
                         newEvent = new SeasonEventDto(
-                            event.returnValues["currentSeason"]);
+                            event.returnValues[this.CURRENT_SEASON]);
                         break;
                     default:
                         this.log.e("The parameter 'type' is not valid");
                 }
                 this.dispatcher.dispatch(newEvent).subscribe(res => this.log.d("New event processed"));
+            } else if (error){
+                this.log.e("Error registering the event listener", error);
             }
         };
 
         this.contract = new this.web3.eth.Contract(this.jsonContractData.abi, this.contractAddress);
+        let block = initialization ? this.FIRST_BLOCK : this.LATEST;
+
         switch (type) {
             case this.COMMIT:
-                this.contract.events.UserNewCommit({ fromBlock: "latest" }, callback);
+                this.contract.events.UserNewCommit({ fromBlock: block }, callback);
                 break;
             case this.REVIEW:
-                this.contract.events.UserNewReview({ fromBlock: "latest" }, callback);
+                this.contract.events.UserNewReview({ fromBlock: block }, callback);
                 break;
             case this.SEASON:
-                this.contract.events.SeasonEnds({ fromBlock: "latest" }, callback);
+                this.contract.events.SeasonEnds({ fromBlock: block }, callback);
                 break;
             default:
                 this.log.e("The parameter 'type' is not valid");
@@ -101,12 +114,12 @@ export class EventHandlerService {
         this.eventsSubscription();
     }
 
-    private eventsSubscription() {
+    private eventsSubscription(initialization = false) {
         this.log.d("Setting the event subscriptions");
-        this.registerNewListener(this.COMMIT);
-        this.registerNewListener(this.REVIEW);
+        this.registerNewListener(this.COMMIT, initialization);
+        this.registerNewListener(this.REVIEW, initialization);
         let provider = this.web3.currentProvider;
-        provider.on("close", e => this.handlerDisconnects(e));
-        provider.on("end", e => this.handlerDisconnects(e));
+        provider.on(this.LISTENER_CLOSE_EVENT, e => this.handlerDisconnects(e));
+        provider.on(this.LISTENER_END_EVENT, e => this.handlerDisconnects(e));
     }
 }
