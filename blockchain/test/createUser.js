d429b415e705114d92862766fb8f08d3c98abec5
Alvaro <alvarogharo@gmail.com>
2020-03-20 14:05:17 +0100

    Added asserts to user creation tests + minor refactor

--- blockchain/test/createUser.js
+++ blockchain/test/createUser.js
@@ -4,9 +4,9 @@ const email = "manuel@example.com";
 
 contract("Bright", accounts => {
     let account = accounts[8];
-    let brightInstance;
-    it("should create account if it is not created yet", () => 
-        Bright.deployed()
+    it("should create account if it is not created yet", () => {
+        let brightInstance;
+        return Bright.deployed()
             .then(instance => {
                 brightInstance = instance;
                 return brightInstance.getUsersAddress();
@@ -23,17 +23,24 @@ contract("Bright", accounts => {
             .then(addresses => {
                 let isAddressCreated = addresses.find(address => address === account) === account;
                 assert(isAddressCreated, "Address was not created correctly");
-            })
+                return brightInstance.getUser(account)
+            }).then(userDetails => {
+                assert.equal(userDetails[0], name);
+                assert.equal(userDetails[1], email);
+            });
+        }
     );
 
-    it("should give error creating an already created account", () => 
-        Bright.deployed()
+    it("should give error creating an already created account", () => {
+        let brightInstance;
+        return Bright.deployed()
             .then(instance => {
                 brightInstance = instance;
                 return brightInstance.setProfile(name, email, { from: account });
             })
             .catch(error => {
                 assert(true);
-            })
+            });
+        }
     );
 });
