61414ffa4e030da60e48a68da4a46929d2c28b1c
Alvaro <alvarogharo@gmail.com>
2020-03-30 12:18:41 +0200

    Solved minor backend bug

--- backend/src/dto/user-details.dto.ts
+++ backend/src/dto/user-details.dto.ts
@@ -13,8 +13,8 @@ export class UserDetailsDto {
         let user = new UserDetailsDto(); 
         user.name = userVals[0];
         user.email = userVals[1];
-        user.numberOfCommits = parseInt(userVals[2]);
-        user.finishedReviews = parseInt(userVals[3]);
+        user.finishedReviews = parseInt(userVals[2]);
+        user.numberOfCommits = parseInt(userVals[3]);
         user.agreedPercentage = userVals[4];
         user.userHash = userVals[5];
         return user; 
