b75f1fcbea043e678176259ce892a14b7e594b44
Alvaro <agomez@tribalyte.com>
2020-04-28 10:06:00 +0200

    Solved twitter share issues

--- webapp/src/components/twitter/twitter.component.ts
+++ webapp/src/components/twitter/twitter.component.ts
@@ -1,27 +1,16 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "twitter-share",
     templateUrl: "twitter.component.html"
 })
 
-export class TwitterComponent implements OnChanges{
+export class TwitterComponent {
     @Input() public url = location.href;
     @Input() public text = "";
     @Input() public hashtag = "";
 
-    constructor() {
-        const auxUrl = "https://platform.twitter.com/widgets.js";
-        if (!document.querySelector(`script[src='${auxUrl}']`)) {
-            let script = document.createElement("script");
-            script.src = auxUrl;
-            document.body.appendChild(script);
-        }
-    }
-
-    public ngOnChanges(): void {
-        if(window["twttr"]) {
-            window["twttr"].widgets.load();
-        }
+    public shareInBlank(){
+        window.open("http://twitter.com/share?text=" + this.text + "&url=" + this.url + "&hashtags=" + this.hashtag, "_blank");
     }
 }
