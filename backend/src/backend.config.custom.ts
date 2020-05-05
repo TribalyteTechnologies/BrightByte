3a8e8dff004e73fdc98bae42de08ebcc43f45d47
Alvaro <agomez@tribalyte.com>
2020-05-05 14:03:31 +0200

    Added global npm start command

--- backend/src/backend.config.custom.ts
+++ backend/src/backend.config.custom.ts
@@ -1,3 +1,3 @@
 export default {
-    NODE_WEBSOCKET_URL : process.env.NODE_WEBSOCKET_URL || "ws://localhost:8945"
+    NODE_WEBSOCKET_URL : process.env.NODE_WEBSOCKET_URL || "ws://localhost:7545"
   };
