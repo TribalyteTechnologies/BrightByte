9feb3eea1d32c5fa5b706312c67839374f4ac1e5
Ronny <rdemera@tribalyte.com>
2020-06-03 11:38:02 +0200

    Enable HTTPS configuration on Backend

--- backend/src/backend.config.ts
+++ backend/src/backend.config.ts
@@ -23,6 +23,7 @@ export class BackendConfig {
     public static readonly NODE_CONFIG_URL = NodeConfig.NODE_WEBSOCKET_URL;
     public static readonly WEBAPP_URL = process.env.WEBAPP_URL || "http://localhost:8100";
     public static readonly BRIGHTBYTE_DB_PORT = process.env.PORT || 3000;
+    public static readonly BRIGHTBYTE_DB_SECURE_PORT = process.env.SECURE_PORT || 3443;
     public static readonly BRIGHT_CONTRACT_URL = BackendConfig.WEBAPP_URL + "/assets/build/Bright.json";
     public static readonly COMMITS_CONTRACT_URL = BackendConfig.WEBAPP_URL + "/assets/build/Commits.json";
     public static readonly CLOUD_EVENT_DISPATCHER_CONTRACT_URL = BackendConfig.WEBAPP_URL + "/assets/build/CloudEventDispatcher.json";
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,37 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { BackendConfig } from "./backend.config";
+import express from "express";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import * as fs from "fs";
+import http from "http";
+import https from "https";
 
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({"origin": BackendConfig.WEBAPP_URL});
-    await app.listenAsync(BackendConfig.BRIGHTBYTE_DB_PORT);
+class BrightByteCloudBackend {
+
+    private readonly DB_PORT = BackendConfig.BRIGHTBYTE_DB_PORT;
+    private readonly DB_SECURE_PORT = BackendConfig.BRIGHTBYTE_DB_SECURE_PORT;
+
+    public async launch() {
+
+        const server = express();
+        const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+        app.enableCors({ "origin": BackendConfig.WEBAPP_URL });
+
+        await app.init();
+
+        const credentials = {
+            key: this.readSecretsFile("private.key"),
+            cert: this.readSecretsFile("certificate.crt")
+        };
+        // HTTP + HTTPS servers
+        http.createServer(server).listen(this.DB_PORT);
+        https.createServer(credentials, server).listen(this.DB_SECURE_PORT);
+    }
+
+    private readSecretsFile(fileName: string): string {
+        return fs.readFileSync(`./secrets/${fileName}`, { encoding: "utf8" });
+    }
 }
-bootstrap();
+
+new BrightByteCloudBackend().launch();
