7e5e1ef7661efd65ea74f8be125158d402acdf75
Ronny <rdemera@tribalyte.com>
2020-07-09 09:37:21 +0200

    Change email template to new design

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -6,25 +6,31 @@ import { map, catchError } from "rxjs/operators";
 import { SuccessResponseDto } from "../dto/response/success-response.dto";
 import { FailureResponseDto } from "../dto/response/failure-response.dto";
 import { BackendConfig } from "../backend.config";
+import * as fs from "fs";
 
 @Injectable()
 export class EmailService {
 
     private readonly INVITATION_SUBJECT = "Invitation to participate on BrightByte";
     private readonly FROM_EMAIL = BackendConfig.EMAIL_TRANSPORT.auth.user;
-    private readonly INVITATION_TEMPLATE = "invitation";
+    private readonly INVITATION_TEMPLATE = "/invitation.html";
 
     constructor(private mailerService: MailerService) { }
 
     public sendInvitationEmail(toEmail: string): Observable<ResponseDto> {
+        let content = this.readFileFrom(BackendConfig.EMAIL_TEMPLATES + this.INVITATION_TEMPLATE);
         return from(this.mailerService.sendMail({
             to: toEmail,
             from: this.FROM_EMAIL,
             subject: this.INVITATION_SUBJECT,
-            template: this.INVITATION_TEMPLATE
+            html: content
         })).pipe(
             map(response => new SuccessResponseDto("The invitation has been send to: " + toEmail)),
             catchError(error => of(new FailureResponseDto(error)))
         );
+    }
+
+    private readFileFrom(filePath: string): string {
+        return fs.readFileSync(filePath, { encoding: "utf8" });
     }
 }
