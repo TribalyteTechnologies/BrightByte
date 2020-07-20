329d0c23ff7cb73a447fa172b7018e27b7cab696
Alvaro <agomez@tribalyte.com>
2020-07-20 14:25:56 +0200

    Minor naming mod

--- webapp/src/domain/contract-manager.service.ts
+++ webapp/src/domain/contract-manager.service.ts
@@ -569,7 +569,7 @@ export class ContractManagerService {
             })
             .catch(error => {
                 let ret: Promise<UserReputation>;
-                if (AppConfig.ERROR_IDENTIFIERS.some(error_id => error_id === error.message)){
+                if (AppConfig.ERROR_IDENTIFIERS.some(errorId => errorId === error.message)){
                     ret =  this.getUserReputationRecursive(contractArtifact , userAddress, season, global, iterationIndex + 1);
                 } else {
                     this.log.e("Error getting reputation:", error);
