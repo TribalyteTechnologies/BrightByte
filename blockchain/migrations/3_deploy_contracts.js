bc19cd3815fce0325137cc96c085cccfd9eb4441
Ronny <rdemera@tribalyte.com>
2020-07-20 04:45:07 +0200

    Added Proxy to up-gradable contracts

--- blockchain/migrations/3_deploy_contracts.js
+++ blockchain/migrations/3_deploy_contracts.js
@@ -8,10 +8,12 @@ var BrightModels = artifacts.require("./BrightModels.sol");
 var UtilsLib = artifacts.require("./UtilsLib.sol");
 var CloudTeamManager = artifacts.require("./CloudTeamManager.sol");
 var CloudBBFactory = artifacts.require("./CloudBrightByteFactory.sol");
+var CloudProjectStore = artifacts.require("./CloudProjectStore.sol");
 var BrightDeployerLib = artifacts.require("./BrightDeployerLib.sol");
 var CommitsDeployerLib = artifacts.require("./CommitsDeployerLib.sol");
 var BrightByteSettingsDeployerLib = artifacts.require("./BrightByteSettingsDeployerLib.sol");
 var RootDeployerLib = artifacts.require("./RootDeployerLib.sol");
+var Proxy = artifacts.require("./contracts/openzeppelin/upgradeability/AdminUpgradeabilityProxy.sol");
 var scVersionObj = require("../../version.json");
 
 const TEAM_UID = 1;
@@ -19,7 +21,9 @@ const USER_ADMIN = "0x0000000000000000000000000000000000000000";
 const SEASON_LENGTH_DAYS = 15;
 var currentVersion = scVersionObj.version;
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
+
+    console.log("CloudBBFactory deployed: ", accounts);
 
     await deployer.link(BrightModels, Bright);
     await deployer.link(UtilsLib, Bright);
@@ -41,4 +45,25 @@ module.exports = async function(deployer) {
     let bbFactory = await deployer.deploy(CloudBBFactory);
     await bbFactory.initialize(currentVersion, CloudTeamManager.address);
     await teamManager.initialize(CloudBBFactory.address, SEASON_LENGTH_DAYS);
+
+    let cloudBBFactory = await CloudBBFactory.new("0x0000000000000000000000000000000000000000");
+    let proxyCloudBBFactory = await Proxy.new(cloudBBFactory.address, accounts[0], []);
+    cloudBBFactory = await CloudBBFactory.at(proxyCloudBBFactory.address);
+    console.log("CloudBBFactory deployed: ", cloudBBFactory.address);
+
+    let cloudTeamManager = await CloudTeamManager.new("0x0000000000000000000000000000000000000000");
+    let proxyCloudTeamManager = await Proxy.new(cloudTeamManager.address, accounts[0], []);
+    cloudTeamManager = await CloudTeamManager.at(proxyCloudTeamManager.address);
+    console.log("CloudTeamManager deployed: ", proxyCloudTeamManager.address);
+
+    let cloudProjectStore = await CloudProjectStore.new("0x0000000000000000000000000000000000000000");
+    let proxyCloudProjectStore = await Proxy.new(cloudProjectStore.address, accounts[0], []);
+    cloudProjectStore = await CloudProjectStore.at(proxyCloudProjectStore.address);
+    console.log("CloudProjectStore deployed: ", cloudProjectStore.address);
+
+
+    await cloudBBFactory.initialize(currentVersion, cloudTeamManager.address, {from: accounts[1]});
+    await cloudTeamManager.initialize(cloudBBFactory.address, SEASON_LENGTH_DAYS, {from: accounts[1]});
+    await cloudProjectStore.initialize(cloudTeamManager.address, {from: accounts[1]});
+  
 };
