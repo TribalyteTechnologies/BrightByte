344f7a63cf7e02f60787e0f8f994e9b6ec2807f5
Ronny <rdemera@tribalyte.com>
2020-07-29 14:03:53 +0200

    Updated update script

--- blockchain/migrations/updateContracts.js
+++ blockchain/migrations/updateContracts.js
@@ -1,21 +1,31 @@
+const Web3 = require("web3");
 var contract = require("truffle-contract");
 var CloudTeamManagerJson = require("../build/contracts/CloudTeamManager.json");
 var ProxyJson = require("../build/contracts/AdminUpgradeabilityProxy.json");
 var CloudBBFactoryJson = require("../build/contracts/CloudBrightByteFactory.json");
-
 var Proxy = contract(ProxyJson);
 var CloudTeamManager = contract(CloudTeamManagerJson);
 var CloudBBFactory = contract(CloudBBFactoryJson);
 
-module.exports = async function (deployer, network, accounts) {
+const OWNER_ADDRESS = "0x0";
+var provider = new Web3.providers.HttpProvider("http://localhost:7545");
+
+
+async function start() {
     let cloudTeamManagerAddress = "0x0";
     let cloudBBFactoryAddress = "0x0";
     await updateContract(CloudTeamManager, cloudTeamManagerAddress);
     await updateContract(CloudBBFactory, cloudBBFactoryAddress);
-};
-
-async function updateContract(contract, proxyContractAddress) {
-    let newContract = await contract.new();
-    let proxyContract = await Proxy.at(proxyContractAddress);
-    await proxyContract.upgradeTo(newContract.address);
 }
+
+async function updateContract(contractType, proxyContractAddress) {
+    console.log("Old contract address is: ", proxyContractAddress);
+    contractType.setProvider(provider);
+    let newContract = await contractType.new({from: OWNER_ADDRESS });
+    let proxyContract = await Proxy.at(proxyContractAddress);
+    await proxyContract.upgradeTo(newContract.address, {from: OWNER_ADDRESS });
+    console.log("New contract address is: ", newContract.address);
+}
+
+Proxy.setProvider(provider);
+start();
