4edcba570af4cd9575c37b7512354355f839db8d
Ronny <rdemera@tribalyte.com>
2020-10-22 18:49:27 +0200

    Back-end retry to connect websocket to geth

--- backend/src/services/web3.service.ts
+++ backend/src/services/web3.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { BackendConfig } from "../backend.config";
 import { ILogger, LoggerService } from "../logger/logger.service";
 import Web3 from "web3";
-import { from, Observable } from "rxjs";
-import { map, catchError } from "rxjs/operators";
 
 @Injectable()
 export class Web3Service {
@@ -16,19 +14,8 @@ export class Web3Service {
         this.log = loggerSrv.get("Web3Service");
     }
 
-    public openConnection(): Observable<Web3> {
+    public openConnection(): Web3 {
         this.log.d("Opening a new Wesocket connection via Web3");
-        let auxWeb3 = new Web3(new Web3.providers.WebsocketProvider(BackendConfig.NODE_CONFIG_URL));
-        return from(auxWeb3.eth.net.isListening())
-        .pipe(
-            map(res => {
-                this.log.d("Open connection ", res);
-                return auxWeb3;
-            }),
-            catchError(error => {
-                this.log.e("Not able to open a connection: ", error);
-                throw error;
-            })
-        );
+        return new Web3(new Web3.providers.WebsocketProvider(BackendConfig.NODE_CONFIG_URL));;
     }
 }
