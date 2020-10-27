1fae507896f5aa43bddacfde0bdfffb36b8211de
Ronny <rdemera@tribalyte.com>
2020-10-27 10:09:28 +0100

    Adapted achievement service

--- webapp/src/domain/achievement.service.ts
+++ webapp/src/domain/achievement.service.ts
@@ -6,6 +6,9 @@ import { Achievement } from "../models/achievement.model";
 import { AchievementPopOver } from "../pages/achievementpopover/achievementpopover";
 import { AppConfig } from "../app.config";
 import { Observable } from "rxjs";
+import { ContractManagerService } from "../domain/contract-manager.service";
+import { from } from "rxjs/observable/from";
+import { flatMap, map } from "rxjs/operators";
 
 interface IAchievementResponse {
     data: Array<any>;
@@ -19,13 +22,15 @@ export class AchievementService {
     public readonly REVIEW_ID = 1;
 
     private readonly NUMBER_OF_ACHIEVEMENTS = 18;
-    private readonly REQ_ROUTE = "/database/achievements/";
+    private readonly REQ_ROUTE = AppConfig.SERVER_BASE_URL + "/database/achievements/";
     private log: ILogger;
     private achievements = new Array<Achievement>();
+    private currentVersion: string;
 
     constructor(
         loggerSrv: LoggerService,
         private popoverCtrl: PopoverController,
+        private contractManagerService: ContractManagerService,
         private http: HttpClient
     ) {
         this.log = loggerSrv.get("AchievementService");
@@ -33,33 +38,37 @@ export class AchievementService {
 
     public getCurrentUnlockedAchievements(userHash: string, teamUid: number): Observable<Array<Achievement>> {
         this.log.d("Request to get unlocked achievements for user", userHash);
-        let achievementsObservable: Observable<Array<Achievement>>;
-        let currentAchievements = new Array<Achievement>();
 
-        achievementsObservable = this.http.get(AppConfig.SERVER_BASE_URL + this.REQ_ROUTE + userHash + "/" + teamUid)
-        .map((response: IAchievementResponse) => {
+        let achievementsObservable = from(this.contractManagerService.getCurrentVersionFromBase()).pipe(
+            flatMap((res: string) => {
+                this.currentVersion = res;
+                const url = this.REQ_ROUTE + userHash + "/" + teamUid + "/" + this.currentVersion;
+                this.log.w("Get url for achivemetns is ", url);
+                return this.http.get(url);
+            }),
+            map((response: IAchievementResponse) => {
+                let currentAchievements = new Array<Achievement>();
                 if (response && response.status === AppConfig.STATUS_OK) {
                     for (let achievement of response.data) {
                         if (achievement) {
                             currentAchievements.push(
-                            new Achievement(
-                                false,
-                                achievement.title,
-                                achievement.values[0],
-                                achievement.parameter,
-                                achievement.iconPath,
-                                achievement.processorType)
+                                new Achievement(
+                                    false,
+                                    achievement.title,
+                                    achievement.values[0],
+                                    achievement.parameter,
+                                    achievement.iconPath,
+                                    achievement.processorType)
                             );
                         }
                     }
                 }
-
                 for (let i = currentAchievements.length; i < this.NUMBER_OF_ACHIEVEMENTS; i++) {
                     currentAchievements.push(new Achievement());
                 }
                 return currentAchievements;
-            });
-
+            })
+        );
         return achievementsObservable;
     }
 
