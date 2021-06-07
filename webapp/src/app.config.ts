610ae1e2500eb71935fa98029c74777d76bdff67
Ronny <rdemera@tribalyte.com>
2021-06-07 09:20:05 +0200

    Fix EXECUTION_ABORTED message

--- webapp/src/app.config.ts
+++ webapp/src/app.config.ts
@@ -71,7 +71,7 @@ export class AppConfig {
     public static readonly MIN_SEASON_LENGTH_DAYS = 1;
     public static readonly VALUES_ARENT_VALID_ERROR_IDENTIFIER = "Returned values aren't valid, did it run Out of Gas?";
     public static readonly NOT_CONVERTIBLE_STRING_ERROR_IDENTIFIER = "The returned value is not a convertible string";
-    public static readonly EXECUTION_ABORTED = "execution aborted (timeout = 5s)";
+    public static readonly EXECUTION_ABORTED = "Returned error: execution aborted (timeout = 5s)";
     public static readonly ERROR_IDENTIFIERS = [
         AppConfig.NOT_CONVERTIBLE_STRING_ERROR_IDENTIFIER, AppConfig.VALUES_ARENT_VALID_ERROR_IDENTIFIER, AppConfig.EXECUTION_ABORTED];
     
